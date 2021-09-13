5bbd812e3d5cb2c9429ff42542e743dda312d006
ian-james-johnson <ian.james.johnson.01@gmail.com>
2021-09-13 16:01:58 -0500

    answering questions

--- acquire.py
+++ acquire.py
@@ -40,3 +40,31 @@ def get_telco_data():
             df.to_csv('telco.csv')
         return df
 
+
+
+def new_zillow_data():
+    '''
+    This function gets new telco data from the Codeup database.
+    '''
+    sql_query = """
+                SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
+                FROM properties_2017
+                JOIN propertylandusetype USING (propertylandusetypeid)
+                WHERE propertylandusedesc = 'Single Family Residential';
+                """
+    # Read in dataframe from Codeup
+    df = pd.read_sql(sql_query,get_connection('zillow'))
+    return df
+
+
+
+def get_zillow_data():
+        '''
+        This function gets telco data from csv, or otherwise from Codeup database.
+        '''
+        if os.path.isfile('zillow.csv'):
+            df = pd.read_csv('zillow.csv', index_col = 0)
+        else:
+            df = new_zillow_data()
+            df.to_csv('zillow.csv')
+        return df
